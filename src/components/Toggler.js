ab8c34115257ee68df461f4f6eb613b235c2f6c7
Brandon <brandonbgrayson@gmail.com>
2021-04-23 18:50:41 -0400

    added toggler function for isHappy

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import {text} from "@chakra-ui/react"
 
 export default function Toggler() {
     const [isHappy, setisHappy] = useState(true)
+    const toggle = () => {
+        setisHappy(!isHappy)
+    }
     return (
         <div>
             <h1>Toggler Goes Here</h1>
-            <h1 onClick={() => setisHappy(!isHappy)}>{isHappy ? "😁" : "😢"}</h1>
+            <h1 onClick={toggle}>{isHappy ? "😁" : "😢"}</h1>
             <h1>something else</h1>
         </div>
     )
