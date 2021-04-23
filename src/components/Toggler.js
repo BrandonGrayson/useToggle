2024a240440996c4f7c9d1de88e7d663db7e3784
Brandon <brandonbgrayson@gmail.com>
2021-04-23 19:00:47 -0400

    added isLoved variable, able to toggle isLoved

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
-import {text} from "@chakra-ui/react"
+import { text } from "@chakra-ui/react"
 
 export default function Toggler() {
     const [isHappy, setisHappy] = useState(true)
+    const [isLoved, setIsLoved] = useState(true)
     const toggle = () => {
         setisHappy(!isHappy)
+    }
+    const toggleIsLoved = () => {
+        setIsLoved(!isLoved)
     }
     return (
         <div>
             <h1>Toggler Goes Here</h1>
             <h1 onClick={toggle}>{isHappy ? "😁" : "😢"}</h1>
+            <h1 onClick={toggleIsLoved} >{isLoved ? "❤️" : "💔"}</h1>
             <h1>something else</h1>
         </div>
     )
