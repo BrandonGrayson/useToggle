e1918f5709eb46c42ff93c277f05667490c3fd75
Brandon <brandonbgrayson@gmail.com>
2021-04-27 08:59:30 -0400

    Contact Form can now take in an input and display it to the screen

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function ContactForm() {
     const [email, setEmail] = React.useState("");
+    const handleInput = (event) => {
+        setEmail(event.target.value)
+    }
     return(
         <div>
-           <h1>The value is ...</h1>
-           <input type='text' placeholder='Email' value={email} />
+           <h1>The value is ... {email}</h1>
+           <input type='text' placeholder='Email' value={email} onChange={handleInput} />
         </div>
-        
     )
 }
